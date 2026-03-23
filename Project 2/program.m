5968b2c56379a5b4ac0d0f2315bd3fc2276429e8
Angelos Kamariadis <150456060+akamariadis@users.noreply.github.com>
2026-03-24 00:47:09 +0200

    Update program.m

--- Project 2/program.m
+++ Project 2/program.m
@@ -41,3 +41,33 @@ figure;
 bode(sys_disturbance);
 grid on;
 title('Διάγραμμα Bode Κλειστού Βρόχου (Διαταραχή -> Έξοδος)');
+
+clear;
+close all;
+clf;
+clc;
+
+A = [-2, -0.02; 1, -10];
+B = [2; 0];
+C = [0, 1];
+Bd = [0; 100];
+
+A_aug = [A, zeros(2,1); -C, 0];
+B_aug = [B; 0];
+Bd_aug = [Bd; 0];
+C_aug = [C, 0];
+D_aug = 0;
+
+P = [-10, -15, -20]; 
+K_aug = place(A_aug, B_aug, P);
+
+A_cl = A_aug - B_aug * K_aug;
+sys_dist_int = ss(A_cl, Bd_aug, C_aug, D_aug);
+
+disp('Συνάρτηση Μεταφοράς από d σε y (με ολοκληρωτή):');
+tf(sys_dist_int)
+
+figure;
+bode(sys_dist_int);
+grid on;
+title('Bode Διαταραχής -> Εξόδου (Με Ολοκληρωτή)');
